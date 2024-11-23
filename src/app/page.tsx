4bbb47c4e1fbfe3bfea6781b3076a0433b264cc6
Jazim Abbas <jazimabbas87@gmail.com>
2024-11-23 18:25:53 +0500

    Added ShadCn UI setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <>
-      <p>ChallengeHive Platform - LeetCode Clone</p>
-    </>
+    <div className="h-[100vh] w-full">
+      <div className="flex h-full flex-col items-center justify-center">
+        <p>ChallengeHive Platform - LeetCode Clone</p>
+        <Button className="w-fit">CTA</Button>
+      </div>
+    </div>
   );
 }
